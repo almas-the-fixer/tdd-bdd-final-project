6e654df7029c689ee437585f884931b391c5708d
AlmasTheFixer <131228605+almas-the-fixer@users.noreply.github.com>
2025-10-26 12:35:30 +0530

    fixed find by name

--- tests/test_models.py
+++ tests/test_models.py
@@ -169,6 +169,18 @@ class TestProductModel(unittest.TestCase):
         for product in found:
             self.assertEqual(product.available, available)
 
+    def test_find_by_name(self):
+        """It should Find Products by Name"""
+        products = ProductFactory.create_batch(10)
+        for product in products:
+            product.create()
+        name = products[0].name
+        count = len([product for product in products if product.name == name])
+        found = Product.find_by_name(name)
+        self.assertEqual(found.count(), count)
+        for product in found:
+            self.assertEqual(product.name, name)
+    
     def test_find_by_category(self):
         """It should Find Products by Category"""
         products = ProductFactory.create_batch(10)
